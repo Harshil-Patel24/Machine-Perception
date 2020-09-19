1fa66f638e01b19b854fb6ee26b8dd35e69b2b6f
Cookie-Monster-420 <58762220+Cookie-Monster-420@users.noreply.github.com>
2020-09-19 11:28:34 +0800

    flat array made

--- Prac5/ex1.py
+++ Prac5/ex1.py
@@ -18,14 +18,14 @@ print("Number of Columns: " + str(num_cols))
 # print("Number of Sections: " + str(num_sections))
 
 digits = [[0 for x in range(0, num_cols)] for y in range(0, num_rows)]
-
+digits_flat = digits.copy()
 for ii in range(0, num_rows, 5):
     for jj in range(0, num_cols):
         # digits[ii][jj] = img[int(ii * height / num_rows):int(ii * height / num_rows + height / num_rows),
         #                  int(jj * height / num_cols):int(jj * width / num_cols + width / num_cols)]
 
         digits[int(ii / 5)][jj] = img[ii*GRID_SIZE:ii*GRID_SIZE + GRID_SIZE, jj*GRID_SIZE:jj*GRID_SIZE + GRID_SIZE]
-
+        digits_flat[int(ii / 5)][jj] = np.asarray(digits[int(ii / 5)][jj]).flatten()
 
 
 # for ii in range(0, len(digits)):
