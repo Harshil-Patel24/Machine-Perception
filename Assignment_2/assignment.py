62c68c9c56e0fd6bd2faa956786b03a611e752d2
Harshil Patel <harshilpatel.personal@gmail.com>
2020-10-08 11:48:14 +0800

    Read in all training data

--- Assignment_2/assignment.py
+++ Assignment_2/assignment.py
@@ -1,25 +1,17 @@
 import cv2 as cv
+import os
 from processing import *
 
 def main():
-    # training_filenames = ['train/h.jpg', 'train/houseNumber_Gr3_06.jpg', \
-    #     'train/houseNum_Gr1_04.jpg', 'train/houseNum_Gr1_06.jpg', 'train/houseNum_Gr1_10.jpg', \
-    #     'train/houseNum_Group2_03.png', 'train/houseNum_Group2_08.png', \
-    #     'train/houseNum_Group2_10.png', 'train/houseNum_Group2_13.png', \
-    #     'train/houseNum_Group2_14.png', 'train/houseNum_Group2_16.jpg', \
-    #     'train/houseNum_Group2_17.jpg', 'train/houseNum_Group2_20.jpg', \
-    #     'train/houseNum_Group2_21.png', 'train/houseNum_Group2_28.jpg', \
-    #     'train/houseNum_Group2_31.jpg', 'train/houseNum_Group2_32.jpg', \
-    #     'train/houseNum_Group2_33.jpg', 'train/houseNum_Group2_37.jpg', \
-    #     'train/houseNum_Group2_38.jpg', 'train/houseNum_Group2_39.jpg', \
-    #     'train/houseNum_Group2_40.jpg', 'train/houseNum_Group2_42.jpg', \
-    #     'train/houseNum_Group2_43.jpg']
+	
+	training = []
 
-    # training = []
-
-    # for name in training_filenames:
-    #     training.append(cv.imread(name))    
-    
-
+	# Read in our training dataset
+	for fname in os.listdir('train'):
+		if fname.endswith('.jpg') or fname.endswith('.png'):
+			training.append(cv.imread('train/' + fname))
+	
+	
+	
 if __name__ == "__main__":
     main()
